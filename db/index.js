a3e5853022f1e5715284c9006a7b766a78732733
Victor Cardins <vcardins@gmail.com>
2017-04-13 12:03:31 -0700

    Removed needed files

--- db/index.js
+++
@@ -1,9 +0,0 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db/db.json');
-const middlewares  = jsonServer.defaults();
-const Console = console;
-
-server.use(middlewares);
-server.use('/api/v1', router)
-server.listen(3333, () => Console.log('JSON Server is running'));
