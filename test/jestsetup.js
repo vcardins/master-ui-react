6e9105ec1fb589b0b8aa1b14c2fa586c362839cb
Victor Cardins <vcardins@gmail.com>
2017-04-25 20:41:01 -0700

    Configured ts-jest and added a sample test (jest & enzyme)

--- test/jestsetup.js
+++ test/jestsetup.js
@@ -1,11 +1,16 @@
-// Make Enzyme functions available in all test files without importing
+// https://github.com/kulshekhar/ts-jest#known-limitations-for-ts-compiler-options
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
 import { shallow, render, mount } from 'enzyme';
-const Console = console;
+
+// Make Enzyme functions available in all test files without importing
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
-// Fail tests on any warning
 
-Console.error = (message) => {
-   throw new Error(message);
-};
+// Fail tests on any warning
+// console.error = (message) => {
+//    throw new Error(message);
+// };
+
+// Reference
+// https://hackernoon.com/testing-react-components-with-jest-and-enzyme-41d592c174f
