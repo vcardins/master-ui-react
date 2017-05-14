91ac28887adff2ce6e70c7f7ed871d9d6bd54168
Victor Cardins <vcardins@gmail.com>
2017-05-14 15:01:20 -0700

    Added react-optmize Babel preset to optimise production generated code

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -30,7 +30,7 @@ const basePlugins = [
     }),
     
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-
+    
     new webpack.DefinePlugin({
         'process.env': {
             NODE_ENV: JSON.stringify(process.env.NODE_ENV),
